737adacc6fa7f561a4942c9a0bf7176c95d43f97
Gerrit Germis <gerrit.germis@gmail.com>
2020-02-18 12:22:07 +0100

    fix default vallue for slow-requests

--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,7 @@ func init() {
 	rootCmd.PersistentFlags().IntVarP(&cli.Option.BucketSize, "bucket-size", "b", 50, "Request time statistics bucket size in ms")
 	rootCmd.PersistentFlags().BoolVarP(&cli.Option.ShowVersion, "version", "v", false, "Show version")
 	rootCmd.PersistentFlags().BoolVarP(&cli.Option.Quiet, "quiet", "q", false, "Be quiet")
-	rootCmd.PersistentFlags().IntVar(&cli.Option.SlowRequests, "slow-requests", 15000, "Indicate slow requests in the dot outputter with an 'S'. Specified in ms")
+	rootCmd.PersistentFlags().IntVar(&cli.Option.SlowRequests, "slow-requests", -1, "Indicate slow requests in the dot outputter with an 'S'. Specified in ms")
 }
 
 var rootCmd = &cobra.Command{
